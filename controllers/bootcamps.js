fa1077de972af7a7ea31a53747be9c733d6d7a01
unknown <tnanh25011997@gmail.com>
2021-01-13 23:18:03 +0700

    geocoder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,33 +1,31 @@
 const Bootcamp = require('../models/Bootcamps')
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-    } catch (error) {
-        res.status(400).json({ success: false });
-    }
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
-}
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+
+
+});
 
 // @desc    Get a single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
-        if (!bootcamp) {
-            return res.status(400).json({ success: false });
-        }
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (error) {
-        res.status(400).json({ success: false });
-    }
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
 
-}
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+
+
+});
 
 // @desc    Create a new bootcamp
 // @route   POST /api/v1/bootcamps
@@ -39,10 +37,8 @@ exports.createBootcamp = async (req, res, next) => {
             success: true,
             data: bootcamp
         });
-    } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+    } catch (error) {
+        next(error);
     }
 
 }
@@ -50,35 +46,31 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
-        if (!bootcamp) {
-            return res.status(400).json({ success: false });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
-        }
-        res.status(200).json({ success: true, data: bootcamp });
-    } catch (error) {
-        return res.status(400).json({ success: false });
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+
     }
+    res.status(200).json({ success: true, data: bootcamp });
 
-}
+
+});
 
 // @desc    Delete a bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-        if (!bootcamp) {
-            return res.status(400).json({ success: false });
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-        }
-        res.status(200).json({ success: true, data: {} });
-    } catch (error) {
-        return res.status(400).json({ success: false });
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+
     }
-}
+    res.status(200).json({ success: true, data: {} });
+
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 //Mount router
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
